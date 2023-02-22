573b77d9e6d45e3af032cde257a47036d3524761
Calvin <calvin@edu.ink>
2023-02-22 07:26:53 -0800

    chore: basic flashcard component

---
+++ src/Flashcard.tsx
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+import './Flashcard.css';
+
+function Flashcard(props: {frontText: string, backText?: string}) {
+  const {frontText = "Front", backText = "Back"} = props;
+  const [isFront, setIsFront] = useState(true);
+  return (
+    <div className="flashcard">
+      <div className={`flashcard-inner ${isFront?"":"flipped"}`}>
+        <div className="flashcard-front">
+          <button className="control" onClick={()=>{setIsFront(false)}}>flip</button>
+          <h2>{frontText}</h2>
+          <p>Some text</p>
+        </div>
+        <div className="flashcard-back">
+          <button className="control" onClick={()=>{setIsFront(true)}}>close</button>
+          <h3>{backText}</h3>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Flashcard;
