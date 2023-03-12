c3b425b2ddf84de403dfd6d65447a5b5e4103ad0
Calvin <calvin@edu.ink>
2023-03-12 10:19:57 -0700

    chore: add more data

--- src/data.tsx
+++ src/data.tsx
@@ -1,16 +1,64 @@
 export interface LearningCard {
+  nodeId: number;
+  /** Chinese character */
   character: string;
   jyutping: string;
-  node_id: number;
 };
 
 export const LearningCards: LearningCard[] = [
-  {character: "山", jyutping: "saan1", node_id: 1},
-  {character: "高", jyutping: "gou1", node_id: 2},
-  {character: "的", jyutping: "dik1", node_id: 3},
-  {character: "人", jyutping: "jan4", node_id: 4},
-  {character: "個", jyutping: "go3", node_id: 5},
-  {character: "大", jyutping: "daai6", node_id: 6},
-  {character: "上", jyutping: "soeng5", node_id: 7},
-  {character: "下", jyutping: "haa6", node_id: 8},
+  {nodeId: 1, character: "山", jyutping: "saan1"},
+  {nodeId: 2, character: "高", jyutping: "gou1"},
+  {nodeId: 3, character: "的", jyutping: "dik1"},
+  {nodeId: 4, character: "人", jyutping: "jan4"},
+  {nodeId: 5, character: "個", jyutping: "go3"},
+  {nodeId: 6, character: "大", jyutping: "daai6"},
+  {nodeId: 7, character: "上", jyutping: "soeng6/soeng5"},
+  {nodeId: 8, character: "下", jyutping: "haa6"},
+  {nodeId: 9, character: "在", jyutping: "zoi6"},
+  {nodeId: 10, character: "有", jyutping: "jau5"},
+
+  {nodeId: 11, character: "手", jyutping: "sau2"},
+  {nodeId: 12, character: "很", jyutping: "han2"},
+  {nodeId: 13, character: "隻", jyutping: "zek3"},
+  {nodeId: 14, character: "小", jyutping: "siu2"},
+  {nodeId: 15, character: "孩", jyutping: "haai4"},
+  {nodeId: 16, character: "家", jyutping: "gaa1"},
+  {nodeId: 17, character: "也", jyutping: "jaa5"},
+  {nodeId: 18, character: "我", jyutping: "ngo5"},
+  {nodeId: 19, character: "指", jyutping: "zi2"},
+  {nodeId: 20, character: "五", jyutping: "ng5"},
+
+  {nodeId: 21, character: "兩", jyutping: "loeng5"},
+  {nodeId: 22, character: "十", jyutping: "sap6"},
+  {nodeId: 23, character: "來", jyutping: "loi4"},
+  {nodeId: 24, character: "是", jyutping: "si6"},
+  {nodeId: 25, character: "女", jyutping: "neoi5"},
+  {nodeId: 26, character: "媽", jyutping: "maa1"},
+  {nodeId: 27, character: "爸", jyutping: "baa1"},
+  {nodeId: 28, character: "馬", jyutping: "maa5"},
+  {nodeId: 29, character: "朋", jyutping: "pang4"},
+  {nodeId: 30, character: "友", jyutping: "jau5"},
+
+  {nodeId: 31, character: "去", jyutping: "heoi3"},
+  {nodeId: 32, character: "這", jyutping: "ze5"},
+  {nodeId: 33, character: "不", jyutping: "bat1"},
+  {nodeId: 34, character: "你", jyutping: "nei5"},
+  {nodeId: 35, character: "們", jyutping: "mun4"},
+  {nodeId: 36, character: "鳥", jyutping: "niu5"},
+  {nodeId: 37, character: "天", jyutping: "tin1"},
+  {nodeId: 38, character: "地", jyutping: "dei6"},
+  {nodeId: 39, character: "花", jyutping: "faa1"},
+  {nodeId: 40, character: "朵", jyutping: "do2/doe2"},
+
+  {nodeId: 41, character: "四", jyutping: "sei3"},
+  {nodeId: 42, character: "草", jyutping: "cou2"},
+  {nodeId: 43, character: "到", jyutping: "dou3/dou2"},
+  {nodeId: 44, character: "說", jyutping: "syut3"},
+  {nodeId: 45, character: "好", jyutping: "hou2"},
+  {nodeId: 46, character: "甚", jyutping: "sam6"},
+  {nodeId: 47, character: "麼", jyutping: "mo1"},
+  {nodeId: 48, character: "飛", jyutping: "fei1"},
+  {nodeId: 49, character: "看", jyutping: "hon3"},
+  {nodeId: 50, character: "裏", jyutping: "leoi5"},
 ];
+
